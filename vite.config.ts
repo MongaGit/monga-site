f59f154fd094309cb269ca75fc98488f735d1a04
pobruno <brunoshy@gmail.com>
2025-02-22 03:53:34 -0300

    chore: move configuração de allowedHosts para .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS ? process.env.VITE_ALLOWED_HOSTS.split(',') : []
   }
 })
